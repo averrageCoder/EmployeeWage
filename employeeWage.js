b6c47bbed6423b27641036607b4eef0a00aafead
Harsh R Jain <rjainharsh@gmail.com>
2021-10-05 23:30:34 +0530

    REFACTORED: used function to get employee hours

--- employeeWage.js
+++ employeeWage.js
@@ -14,18 +14,19 @@ const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR =  20;
 
-let empHrs = 0;
-let empcheck = Math.floor(Math.random() * 10) % 3;
-switch(empcheck) {
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs = 0;
+function getEmployeeHrs(empcheck) {
+    switch(empcheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            return  FULL_TIME_HOURS;
+            break;
+        default:
+            return  0;
+    }
 }
-
-let empWage = empHrs * WAGE_PER_HOUR;
-console.log("UC2- Employee wage: ", empWage);
+let empcheck = Math.floor(Math.random() * 10) % 3;
+let empHrs = getEmployeeHrs(empcheck);
+let empWage =  empHrs * WAGE_PER_HOUR;
+console.log("Employee wage: ", empWage);
