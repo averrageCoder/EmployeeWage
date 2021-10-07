b27ffc760776d052887725d8917f7e08b52ac4ce
Harsh R Jain <rjainharsh@gmail.com>
2021-10-07 14:11:03 +0530

    ADDED: ability to add gender and start date to employee payroll data

---
+++ EmployeePayrollData.js
@@ -0,0 +1,37 @@
+class EmployeePayrllData {
+    id;
+    salary;
+    gender;
+    startDate;
+
+    constructor(...params) {
+        this.id = params[0];
+        this.name = params[1];
+        this.salary = params[2];
+        this.gender = params[3];
+        this.startDate = params[4];
+    }
+
+    get name() {return this._name; }
+    set name(name) {
+        this._name = name;
+    }
+
+    
+
+    toString() {
+        const options = {year: 'numeric', month: 'long', day: 'numeric'};
+        const empDate = !this.startDate ? "not defined" : 
+                        this.startDate.toLocaleDateString("en-US", options);
+        return "id=" + this.id + ", name: "+this.name + ", salary: " + this.salary
+        + ", gender: "+this.gender+", startDate: "+empDate;
+    }
+} 
+
+let employeePayrllData  = new EmployeePayrllData(1, "Mark", 30000);
+employeePayrllData.id = 0;
+employeePayrllData.name = 'Jeff';
+console.log("employeePayrllData: "+employeePayrllData.toString());
+
+let newEmployeePayrllData  = new EmployeePayrllData(1, "Terissa", 30000, 'f', new Date());
+console.log("employeePayrllData: "+newEmployeePayrllData.toString());
