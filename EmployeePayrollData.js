d3f7afa5dc4cac8900e98a7c003ebe28a3009efa
Harsh R Jain <rjainharsh@gmail.com>
2021-10-07 16:49:24 +0530

    ADDED: validation for gender, id, salary and startdate

--- EmployeePayrollData.js
+++ EmployeePayrollData.js
@@ -1,8 +1,4 @@
 class EmployeePayrllData {
-    id;
-    salary;
-    gender;
-    startDate;
 
     constructor(...params) {
         this.id = params[0];
@@ -19,6 +15,36 @@ class EmployeePayrllData {
         else throw 'Name is incorrect';
     }
 
+    get id() {return this._id; }
+    set id(id) {
+        let idRegex = RegExp('^\d*[1-9]\d*$')
+        if(idRegex.test(id)) this._id = id;
+        else 
+            throw 'ID is incorrect';
+    }
+
+    get salary() {return this._salary; }
+    set salary(salary) {
+        let salaryRegex = RegExp('^[0-9.]+$')
+        if(salaryRegex.test(salary)) this._salary = salary;
+        else 
+            throw 'Salary is incorrect';
+    }
+
+    get gender() {return this._gender; }
+    set gender(gender) {
+        let genderRegex = RegExp('^[MFmf]{1}$')
+        if(genderRegex.test(gender)) this._gender = gender;
+        else 
+            throw 'gender is incorrect';
+    }
+
+    get startDate() {return this._startDate; }
+    set startDate(startDate) {
+        if(new Date() < startDate) throw 'Start date is invalid!';
+        else this._startDate = startDate;
+    }
+
     
 
     toString() {
@@ -28,17 +54,11 @@ class EmployeePayrllData {
         return "id=" + this.id + ", name: "+this.name + ", salary: " + this.salary
         + ", gender: "+this.gender+", startDate: "+empDate;
     }
-} 
-
-let employeePayrllData  = new EmployeePayrllData(1, "Mark", 30000);
-employeePayrllData.id = 0;
-try {
-    employeePayrllData.name = 'Jef';
-    console.log("employeePayrllData: "+employeePayrllData.toString());
 }
-catch (e) {
+
+try {
+    let newEmployeePayrllData  = new EmployeePayrllData(1, "Terissa", 30000, 'm', new Date('2022-01-01'));
+    console.log("employeePayrllData: "+newEmployeePayrllData.toString());
+} catch (e) {
     console.error(e);
 }
-
-let newEmployeePayrllData  = new EmployeePayrllData(1, "Terissa", 30000, 'f', new Date());
-console.log("employeePayrllData: "+newEmployeePayrllData.toString());
